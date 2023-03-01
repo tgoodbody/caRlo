2c9d5a340e4ab088b98cbd9964df1af46dcaad19
Tristan Goodbody <goodbody.t@gmail.com>
2023-03-01 15:57:07 -0800

    add 'data' prefix to data files

---
+++ R/data-gedi.R
@@ -0,0 +1,8 @@
+#' GEDI data
+#'
+#' This is an `sf` object containing the spatial data for 1000 GEDI samples in a study area.
+#'
+#' @format `sf` object.
+#' @source This data was collected by the GEDI mission
+#' @keywords datasets
+"gedi"
