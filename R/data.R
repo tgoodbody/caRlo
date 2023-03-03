aa799a91f340b2a783fc07998da8272bcf9fb908
Tristan Goodbody <60200460+tgoodbody@users.noreply.github.com>
2023-03-02 19:17:26 -0800

    document

--- R/data.R
+++ R/data.R
@@ -6,3 +6,35 @@
 #' @source This data was collected by the GEDI mission
 #' @keywords datasets
 "gedi"
+
+#' Plots data
+#'
+#' This is an `sf` object containing the spatial data for 1000 plots with ALS metrics.
+#'
+#' @format `sf` object.
+#' @keywords datasets
+"plots"
+
+#' Monte Carlo samples
+#'
+#' This is an `tibble` of monte carlo samples from the `monte_carlo()` function.
+#'
+#' @format `sf` object.
+#' @keywords datasets
+"samples"
+
+#' Statistics of samples
+#'
+#' This is a nested `tibble` of statistics for `samples`
+#'
+#' @format `sf` object.
+#' @keywords datasets
+"stats"
+
+#' Bootstraps of samples
+#'
+#' This is a nested `tibble` of bootstraps of statistics for `stats`
+#'
+#' @format `sf` object.
+#' @keywords datasets
+"bootstraps"
